f8501fe84110d2c497c930634ef67079d08f0bce
Rebeca <163349981+ChioreanRebeca@users.noreply.github.com>
2025-05-24 20:02:26 +0300

    spring boot initializr

---
+++ sdm-project/src/main/java/com/sdm/sdm_project/SdmProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sdm.sdm_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdmProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdmProjectApplication.class, args);
+	}
+
+}
